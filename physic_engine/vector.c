533548e22040441d9b483a1aa945871975996ef6
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2023-12-12 22:38:42 +0100

    format

--- physic_engine/vector.c
+++ physic_engine/vector.c
@@ -5,7 +5,9 @@
 
 vector const ZERO = {0.0, 0.0};
 
-bool vector__is_zero(vector const *u) { return u->x == 0.0 && u->y == 0.0; }
+bool vector__is_zero(vector const *u) {
+    return u->x == 0.0 && u->y == 0.0;
+}
 
 static double fast_inverse_square_root(double number) {
     long i;
@@ -15,7 +17,7 @@ static double fast_inverse_square_root(double number) {
     x2 = number * 0.5;
     y = number;
     i = *(long *)&y;  // evil doubleing point bit level hacking
-    // i = 0x53759df - (i >> 1); // what the fuck?
+    // i = 0x5f3759df - (i >> 1); // what the fuck?
     i = 0x5fe6eb50c7b537a9 - (i >> 1);  // what the fuck?
     y = *(double *)&i;
     y = y * (threehalfs - (x2 * y * y));
@@ -50,7 +52,9 @@ double vector__dot_product(vector const *u, vector const *v) {
     return (u->x * v->x) + (u->y * v->y);
 }
 
-double vector__squared_norm(vector const *u) { return u->x * u->x + u->y * u->y; }
+double vector__squared_norm(vector const *u) {
+    return u->x * u->x + u->y * u->y;
+}
 
 void vector__iadd(vector *u, vector const *v) {
     u->x += v->x;
@@ -76,4 +80,6 @@ void vector__normalize(vector *u) {
     vector__imul(u, fast_inverse_square_root(vector__squared_norm(u)));
 }
 
-void vector__print(vector const *u) { printf("[%lf, %lf]", u->x, u->y); }
+void vector__print(vector const *u) {
+    printf("[%lf, %lf]", u->x, u->y);
+}
